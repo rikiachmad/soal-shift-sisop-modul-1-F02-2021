f3dfa6b6677303c6fb440d417d2589440926b492
Akbar Irwanda <45300477+Irwanda04@users.noreply.github.com>
2021-03-26 21:40:43 +0700

    update soal2 done
    Selesai, bisa dikoreksi dulu.

--- soal2/soal2_generate_laporan_ihir_shisop.sh
+++ soal2/soal2_generate_laporan_ihir_shisop.sh
@@ -4,6 +4,11 @@ awk -F '\t' '
 
 (NR>1){
 	rowID = $1;
+	orderDate = $3;
+	custName = $7;
+	segment = $8;
+	city = $10;
+	region = $13;
 
 	sales = $18;
 	salesDec = substr(sales, index(sales, ".")+1, length(sales));
@@ -25,7 +30,33 @@ awk -F '\t' '
 		maxID = rowID;
 		curMaxPP = profitPercentage;
 	}
+
+	if(city=="Albuquerque" && substr(orderDate, length(orderDate)-1, 2)=="17")
+		cust[custName]++;
+
+	segCount[segment]++;
+	regCount[region] += profitFix;
 }
 END{
-	print maxID " " curMaxPP;
+	print "Transaksi terakhir dengan profit percentage terbesar yaitu " maxID " dengan persentase " curMaxPP "%.\n";
+
+	print "Daftar nama customer di Albuquerque pada tahun 2017 antara lain:"
+	for(i in cust)
+		print i;
+
+	minSeg = 10000;
+	for(i in segCount)
+		if(segCount[i] < minSeg){
+			minSeg = segCount[i];
+			segType = i;
+		}
+	print "\nTipe segmen customer yang penjualannya paling sedikit adalah " segType " dengan " minSeg " transaksi.\n"
+	
+	minReg = 100000000;
+	for(i in regCount)
+		if(regCount[i] < minReg){
+			minReg = regCount[i];
+			regName = i;
+		}
+	print "\nWilayah bagian (region) yang memiliki total keuntungan (profit) yang paling sedikit adalah " regName " dengan total keuntungan " minReg "."
 }' Laporan-TokoShiSop.tsv > hasil.txt
