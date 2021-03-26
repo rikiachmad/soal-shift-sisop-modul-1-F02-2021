be506a3ebf73136b7c1c1fd97c0720f21865aecc
Akbar Irwanda <45300477+Irwanda04@users.noreply.github.com>
2021-03-26 21:09:06 +0700

    update 2a fix
    error for -0.* is fixed

--- soal2/soal2_generate_laporan_ihir_shisop.sh
+++ soal2/soal2_generate_laporan_ihir_shisop.sh
@@ -9,23 +9,23 @@ awk -F '\t' '
 	salesDec = substr(sales, index(sales, ".")+1, length(sales));
 	salesFix = sales * (10^(length(sales)-index(sales, "."))) + salesDec;
 	salesFix = salesFix/(10^(length(sales)-index(sales, ".")));
+	if(substr(sales,1,3)=="-0.")
+		salesFix = 0-salesFix;
 
 	profit = $21;
 	profitDec = substr(profit, index(profit, ".")+1, length(profit));
 	profitFix = profit * (10^(length(profit)-index(profit, ".")-1)) + profitDec;
 	profitFix = profitFix/(10^(length(profit)-index(profit, ".")-1));
-
+	if(substr(profit,1,3)=="-0.")
+		profitFix = 0-profitFix;
 
 	profitPercentage = (profitFix/(salesFix-profitFix))*100;
 
 	if(profitPercentage >= curMaxPP){
 		maxID = rowID;
 		curMaxPP = profitPercentage;
-		prf = profitFix;
-		sls = salesFix;
 	}
-	print salesFix " " profitFix " " profitPercentage;
 }
 END{
-	print maxID " " curMaxPP " " prf " " sls;
+	print maxID " " curMaxPP;
 }' Laporan-TokoShiSop.tsv > hasil.txt
