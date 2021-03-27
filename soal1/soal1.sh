a137cb01eeaa0b37543c3f786fabf1bce366d4c7
rikiachmad <74702068+rikiachmad@users.noreply.github.com>
2021-03-27 20:56:09 +0700

    Update soal1.sh

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -10,14 +10,14 @@ count="${arr[0]}"
 
 for((i=1; i<${#arr[@]}; i++))
 do
-if ! [[ "${arr[$i]}" =~ ^[0-9]+$ ]]; then
-echo -n "${arr[$i]}" >> error_message.csv;
-if ! [[ "${arr[$(($i+1))]}" =~ ^[0-9]+$ ]] && [ $i -ne $((${#arr[@]}-1)) ]; then
-echo -n  " " >> error_message.csv
-fi
-else echo -n -e ",$count\n" >> error_message.csv
-count=${arr[$i]}
-fi
+  if ! [[ "${arr[$i]}" =~ ^[0-9]+$ ]]; then
+    echo -n "${arr[$i]}" >> error_message.csv;
+  if ! [[ "${arr[$(($i+1))]}" =~ ^[0-9]+$ ]] && [ $i -ne $((${#arr[@]}-1)) ]; then
+    echo -n  " " >> error_message.csv
+  fi
+  else echo -n -e ",$count\n" >> error_message.csv
+    count=${arr[$i]}
+  fi
 done
 echo ",$count" >> error_message.csv
 
@@ -27,9 +27,9 @@ user=`grep -o "ERROR.*\|INFO.*" syslog.log | cut -f2- -d '(' | sed 's/)$//' | so
 data=($user)
 for ((i=0; i<${#data[@]}; i++))
 do
-error=`grep -c "ERROR.*${data[$i]}" syslog.log`
-info=`grep -c "INFO.*${data[$i]}" syslog.log`
-echo -n  "${data[$i]}," >> user_statistic.csv
-echo -n "$info," >> user_statistic.csv
-echo "$error" >> user_statistic.csv
+  error=`grep -c "ERROR.*${data[$i]}" syslog.log`
+  info=`grep -c "INFO.*${data[$i]}" syslog.log`
+  echo -n  "${data[$i]}," >> user_statistic.csv
+  echo -n "$info," >> user_statistic.csv
+  echo "$error" >> user_statistic.csv
 done
